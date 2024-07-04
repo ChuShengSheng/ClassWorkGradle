036a8053f235bf1082f5252628ba08ab5987a5ee
Olga <151966653+ChuShengSheng@users.noreply.github.com>
2024-07-04 12:21:28 +0300

    Add files via upload

---
+++ ClassWorkGradle/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import com.google.common.collect.HashMultimap;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMultimap<Integer, Set<String>> option4 = HashMultimap.create();
+    }
+}
